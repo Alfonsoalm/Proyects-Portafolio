996f91401740d59bc42f0ae326caca8196c955df
Alfonsoalm <alfonsoalm34@gmail.com>
2024-10-27 17:59:42 +0100

    Funcionalidad de correo añadida

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export const Contacto = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    console.log(form.current.user_email.value);
+
+    emailjs
+      .sendForm('service_ts90omb', 'template_l922ieg', form.current, {
+        publicKey: 'bDdum3pdzcVJ35Vij',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+
   return (
-    <>
-      <div className="page">
-        <h1 className="heading">Contact</h1>
-        <hr />
-        <form className="contact" action="mailto:alfonsoalm34@gmail.com">
-          <input type="text" placeholder="Name"></input>
-          <input type="text" placeholder="Surname"></input>
-          <input type="text" placeholder="Email"></input>
-          <textarea placeholder="Reason for contact" />
-          <input type="submit" value="Send"></input>
-        </form>
-      </div>
-    </>
+    <div className="page">
+      <h1 className="heading">Contact</h1>
+      <hr />
+      <form ref={form} onSubmit={sendEmail} className="contact">
+        <input type="text" name="user_name" placeholder="Name" required />
+        <input type="text" name="user_surname" placeholder="Surname" required />
+        <input type="email" name="user_email" placeholder="Email" required />
+        <textarea name="message" placeholder="Reason for contact" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
   );
 };
